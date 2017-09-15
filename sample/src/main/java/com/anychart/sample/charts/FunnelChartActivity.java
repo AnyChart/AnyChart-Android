ec81bf0abfb2c6c9b7dd72bdd26a4d760ed72d55
ArsenyMalkov <arsenymalkov@gmail.com>
2017-09-15 14:03:30 +0800

    Add funnel chart sample

---
+++ sample/src/main/java/com/anychart/sample/charts/FunnelChartActivity.java
@@ -0,0 +1,33 @@
+package com.anychart.sample.charts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.anychart.anychart.AnyChartView;
+import com.anychart.anychart.Funnel;
+import com.anychart.anychart.TextParsingMode;
+import com.anychart.sample.R;
+
+public class FunnelChartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart_common);
+
+        AnyChartView anyChartView = (AnyChartView) findViewById(R.id.any_chart_view);
+
+        Funnel funnel = new Funnel();
+        funnel.setData(new String[] {
+                "['Website Visits', 528756]",
+                "['Downloads', 164052]",
+                "['Valid Contacts', 112167]",
+                "['Interested to Buy', 79128]",
+                "['Purchased', 79128]"
+        }, TextParsingMode.CSV);
+
+        funnel.setMargin(10d, "20%", 10d, "20%", 0d, "0%", 0d);
+
+        anyChartView.setChart(funnel);
+    }
+}
