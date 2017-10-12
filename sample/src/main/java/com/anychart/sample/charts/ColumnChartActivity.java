4a6ef66484e9ac126d335df13c0055df6c7f2acb
ArsenyMalkov <arsenymalkov@gmail.com>
2017-10-12 12:07:02 +0800

    Update column

--- sample/src/main/java/com/anychart/sample/charts/ColumnChartActivity.java
+++ sample/src/main/java/com/anychart/sample/charts/ColumnChartActivity.java
@@ -5,9 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.anychart.anychart.AnyChartView;
 import com.anychart.anychart.Cartesian;
-import com.anychart.anychart.DistinctColors;
-import com.anychart.anychart.LinearGradientFill;
+import com.anychart.anychart.CartesianSeriesColumn;
+import com.anychart.anychart.EnumsAnchor;
+import com.anychart.anychart.HoverMode;
+import com.anychart.anychart.Position;
 import com.anychart.anychart.TextParsingMode;
+import com.anychart.anychart.TooltipPositionMode;
 import com.anychart.sample.R;
 
 public class ColumnChartActivity extends AppCompatActivity {
@@ -20,15 +23,38 @@ public class ColumnChartActivity extends AppCompatActivity {
         AnyChartView anyChartView = (AnyChartView) findViewById(com.anychart.sample.R.id.any_chart_view);
 
         Cartesian cartesian = new Cartesian();
-        cartesian.column(new String[] {"['2014', 100]", "['2015', 120]", "['2016', 180]"}, TextParsingMode.CSV);
-        cartesian.setLabels(true);
-        cartesian.setXAxis(true);
-        cartesian.setYAxis(true);
+        CartesianSeriesColumn column = cartesian.column(new String[] {
+                "['Rouge', '80540']",
+                "['Foundation', '94190']",
+                "['Mascara', '102610']",
+                "['Lip gloss', '110430']",
+                "['Lipstick', '128000']",
+                "['Nail polish', '143760']",
+                "['Eyebrow pencil', '170670']",
+                "['Eyeliner', '213210']",
+                "['Eyeshadows', '249980']"
+        }, TextParsingMode.CSV);
 
-        LinearGradientFill linearGradientFill = new LinearGradientFill(new String[]{"'#456789'", "'#987654'"}, false, 90d, .8d);
-        DistinctColors distinctColors = new DistinctColors();
-        distinctColors.setItems(linearGradientFill, null);
-        cartesian.setPalette(distinctColors);
+        column.getTooltip()
+                .setTitleFormat("'{%X}'")
+                .setPosition(Position.CENTER_BOTTOM)
+                .setAnchor(EnumsAnchor.CENTER_BOTTOM)
+                .setOffsetX(0d)
+                .setOffsetY(5d)
+                .setFormat("'${%Value}{groupsSeparator: }'");
+
+        cartesian.setAnimation(true);
+        cartesian.setTitle("'Top 10 Cosmetic Products by Revenue'");
+
+        cartesian.getYScale().setMinimum(0d);
+
+        cartesian.getYAxis().getLabels().setFormat("'${%Value}{groupsSeparator: }'");
+
+        cartesian.getTooltip().setPositionMode(TooltipPositionMode.POINT);
+        cartesian.getInteractivity().setHoverMode(HoverMode.BY_X);
+
+        cartesian.getXAxis().setTitle("'Product'");
+        cartesian.getYAxis().setTitle("'Revenue'");
 
         anyChartView.setChart(cartesian);
     }
