049e802143f4c57dadee90cb494708c99cf5155e
ArsenyMalkov <arsenymalkov@gmail.com>
2017-10-13 17:05:39 +0800

    Add pareto chart

---
+++ sample/src/main/java/com/anychart/sample/charts/ParetoChartActivity.java
@@ -0,0 +1,70 @@
+package com.anychart.sample.charts;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.anychart.anychart.AnyChartView;
+import com.anychart.anychart.CartesianSeriesBase;
+import com.anychart.anychart.EnumsAnchor;
+import com.anychart.anychart.Pareto;
+import com.anychart.anychart.StrokeLineCap;
+import com.anychart.anychart.StrokeLineJoin;
+import com.anychart.anychart.TextParsingMode;
+import com.anychart.sample.R;
+
+public class ParetoChartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart_common);
+
+        AnyChartView anyChartView = (AnyChartView) findViewById(R.id.any_chart_view);
+
+        Pareto pareto = new Pareto();
+        pareto.setData(new String[] {
+                "{ x: 'Food is tasteless', value: 65 }",
+                "{ x: 'Wait time', value: 109 }",
+                "{ x: 'Unfriendly staff', value: 12.5 }",
+                "{ x: 'Not clean', value: 45 }",
+                "{ x: 'Overpriced', value: 250 }",
+                "{ x: 'To noisy', value: 27 }",
+                "{ x: 'Food not fresh', value: 35 }",
+                "{ x: 'Small portions', value: 170 }",
+                "{ x: 'Not atmosphere', value: 35 }",
+                "{ x: 'Food is to salty', value: 35 }"
+        }, TextParsingMode.CSV);
+
+        pareto.setTitle("'Pareto Chart of Restaurant Complaints'");
+
+        pareto.getYAxis(0d).setTitle("'Defect frequency'");
+
+        pareto.getYAxis(1d).setTitle("'Cumulative Percentage'");
+
+        pareto.setAnimation(true);
+
+        pareto.getLineMarker()
+                .setValue(80d)
+//                .setAxis(pareto.getXAxis(1d))
+                .setStroke("'#A5B3B3'", 1d, "'5 2'", StrokeLineJoin.ROUND, StrokeLineCap.ROUND);
+
+        pareto.getGetSeries(0d).getTooltip().setFormat("'Value: {%Value}'");
+
+//        CartesianSeriesBaseWithMarkers
+        CartesianSeriesBase line = pareto.getGetSeries(1d);
+        line.setSeriesType("'spline'");
+//        line.setMarkers(true);
+
+        line.getLabels().setEnabled(true);
+        line.getLabels()
+                .setAnchor(EnumsAnchor.RIGHT_BOTTOM)
+                .setFormat("'{%CF}%'");
+
+        line.getTooltip().setFormat("'Cumulative Frequency: {%CF}% \\n Relative Frequency: {%RF}%'");
+
+        pareto.getCrosshair().setEnabled(true);
+        pareto.getCrosshair().setXLabel(true);
+
+        anyChartView.setChart(pareto);
+    }
+}
---
+++ sample/src/main/java/com/anychart/sample/charts/PyramidActivity.java
@@ -0,0 +1,41 @@
+package com.anychart.sample.charts;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.anychart.anychart.AnyChartView;
+import com.anychart.anychart.EnumsAlign;
+import com.anychart.anychart.LegendLayout;
+import com.anychart.anychart.Pyramid;
+import com.anychart.anychart.TextParsingMode;
+import com.anychart.anychart.UiLegend;
+import com.anychart.sample.R;
+
+public class PyramidActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chart_common);
+
+        AnyChartView anyChartView = (AnyChartView) findViewById(R.id.any_chart_view);
+
+        Pyramid pyramid = new Pyramid();
+        pyramid.setData(new String[] {
+                "{name: 'TV promotion', value: 6371664}",
+                "{name: 'Radio promotion', value: 7216301}",
+                "{name: 'Advertising leaflets', value: 1486621}",
+                "{name: 'Before advertising started', value: 1386622}"
+        }, TextParsingMode.CSV);
+
+        UiLegend legend = pyramid.getLegend();
+        legend.setEnabled(true);
+        legend.setPosition("'outside-right'");
+        legend.setItemsLayout(LegendLayout.VERTICAL);
+        legend.setAlign(EnumsAlign.TOP);
+
+        pyramid.setLabels(false);
+
+        anyChartView.setChart(pyramid);
+    }
+}
