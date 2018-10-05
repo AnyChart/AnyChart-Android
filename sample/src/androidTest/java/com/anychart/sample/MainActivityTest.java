fff0c02e6bbaa440ff4c584af5bc6c2939ae2791
Arseny Malkov <arsenymalkov@gmail.com>
2018-10-05 11:57:44 +0800

    Minor fix

--- sample/src/androidTest/java/com/anychart/sample/MainActivityTest.java
+++ sample/src/androidTest/java/com/anychart/sample/MainActivityTest.java
@@ -3,9 +3,9 @@ package com.anychart.sample;
 
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.web.webdriver.Locator;
+import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
-import android.test.suitebuilder.annotation.LargeTest;
 
 import org.junit.Rule;
 import org.junit.Test;
